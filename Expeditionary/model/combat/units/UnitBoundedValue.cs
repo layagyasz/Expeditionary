8c3678670328d9fc68c08d910f83f1cebdad3656
layagyasz <zchrylawrence@gmail.com>
2024-10-04 16:14:55 -0600

    Try to reconcile directories

--- Expeditionary/model/combat/units/UnitBoundedValue.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Expeditionary.Model.Combat.Units
-{
-    public struct UnitBoundedValue
-    {
-        public UnitModifier Minimum { get; set; }
-        public UnitModifier Value { get; set; }
-    }
-}
