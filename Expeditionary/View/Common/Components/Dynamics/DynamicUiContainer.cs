af75666984236cd6027cbbfe276ee581d151902c
layagyasz <zchrylawrence@gmail.com>
2025-11-11 15:21:39 -0700

    Improve load bar appearance

---
+++ Expeditionary/View/Common/Components/Dynamics/DynamicUiContainer.cs
@@ -0,0 +1,23 @@
+﻿using Cardamom.Ui.Controller.Element;
+using Cardamom.Ui;
+using Cardamom.Ui.Elements;
+
+namespace Expeditionary.View.Common.Components.Dynamics
+{
+    public class DynamicUiContainer : UiContainer, IDynamic
+    {
+        public DynamicUiContainer(Class @class, IElementController controller)
+            : base(@class, controller) { }
+
+        public virtual void Refresh()
+        {
+            foreach (var element in this)
+            {
+                if (element is IDynamic dynamic)
+                {
+                    dynamic.Refresh();
+                }
+            }
+        }
+    }
+}
--- Expeditionary/View/Common/Components/LoadBar.cs
+++ Expeditionary/View/Common/Components/LoadBar.cs
@@ -1,6 +1,5 @@
 ﻿using Cardamom.Ui.Controller.Element;
 using Cardamom.Ui.Controller;
-using Cardamom.Ui.Elements;
 using Cardamom.Ui;
 using Expeditionary.Loader;
 using Expeditionary.View.Common.Components.Dynamics;
@@ -9,24 +8,29 @@ namespace Expeditionary.View.Common.Components
 {
     public class LoadBar : DynamicUiCompoundComponent
     {
-        private static readonly string s_Container = "load-container";
-        private static readonly string s_Bar = "load-bar";
+        private static readonly string s_Container = "load-bar-outline";
+        private static readonly string s_Bar = "load-bar-fill";
+        private static readonly string s_Text = "load-bar-text";
 
-        private LoadBar(IController controller, IUiContainer container, IUiElement bar)
+        private LoadBar(IController controller, IUiContainer container, IUiElement bar, IUiElement text)
             : base(controller, container)
         {
             Add(bar);
+            Add(text);
         }
 
         public static LoadBar Create(UiElementFactory uiElementFactory, LoaderStatus loaderStatus)
         {
             return new(
                 new NoOpController(), 
-                new DynamicUiSerialContainer(
+                new DynamicUiContainer(
                       uiElementFactory.GetClass(s_Container),
-                      new NoOpElementController(),
-                      UiSerialContainer.Orientation.Vertical),
-                new PoolBar(uiElementFactory.GetClass(s_Bar), new NoOpElementController(), loaderStatus.Progress));
+                      new NoOpElementController()),
+                new PoolBar(uiElementFactory.GetClass(s_Bar), new NoOpElementController(), loaderStatus.Progress),
+                new DynamicTextUiElement(
+                    uiElementFactory.GetClass(s_Text),
+                    new NoOpElementController(),
+                    () => loaderStatus.Progress.PercentFull().ToString("P0")));
         }
     }
 }
