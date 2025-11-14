8e959458d07fe831a302acc9c5cd014fbc73e20e
layagyasz <zchrylawrence@gmail.com>
2025-11-14 11:07:39 -0700

    Apply background to load screen

--- Expeditionary/View/Screens/LoadScreen.cs
+++ Expeditionary/View/Screens/LoadScreen.cs
@@ -17,13 +17,15 @@ namespace Expeditionary.View.Screens
         public event EventHandler<EventArgs>? Updated;
 
         public IController Controller { get; }
+        public TextureBackground Background { get; }
         public LoadBar LoadBar { get; }
 
         private readonly DynamicRefresher _refresher = new(s_RefreshTime);
 
-        private LoadScreen(IController controller, LoadBar loadBar)
+        private LoadScreen(IController controller, TextureBackground background, LoadBar loadBar)
         {
             Controller = controller;
+            Background = background;
             LoadBar = loadBar;
 
             _refresher.Add(LoadBar);
@@ -31,24 +33,30 @@ namespace Expeditionary.View.Screens
 
         public void Draw(IRenderTarget target, IUiContext context)
         {
+            Background.Draw(target, context);
             LoadBar.Draw(target, context);
         }
 
         public void Initialize()
         {
             Controller.Bind(this);
+            Background.Initialize();
             LoadBar.Initialize();
         }
 
         public void ResizeContext(Vector3 bounds)
         {
+
+            Background.ResizeContext(bounds);
             LoadBar.Position = 0.5f * (bounds - LoadBar.Size);
+            LoadBar.ResizeContext(bounds);
         }
 
         public void Update(long delta)
         {
             _refresher.Update(delta);
             LoadBar.Update(delta);
+            Background.Update(delta);
             Updated?.Invoke(this, EventArgs.Empty);
         }
 
@@ -61,8 +69,12 @@ namespace Expeditionary.View.Screens
         public static LoadScreen Create(
             UiElementFactory uiElementFactory, ILoaderTask loaderTask, LoaderStatus loaderStatus)
         {
+            var segment = uiElementFactory.GetTexture("background_main_menu");
             return new LoadScreen(
-                new LoadScreenController(loaderTask), LoadBar.Create(uiElementFactory, loaderStatus));
+                new LoadScreenController(loaderTask),
+                new TextureBackground(
+                    segment.Texture!, segment.TextureView, uiElementFactory.GetShader("shader-default")),
+                LoadBar.Create(uiElementFactory, loaderStatus));
         }
     }
 }
